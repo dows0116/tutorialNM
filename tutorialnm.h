6d957abf1dd35f741bd48b65f6ff6eae9ca07187
dows0116 <84549167+dows0116@users.noreply.github.com>
2021-05-21 10:51:18 +0900

    Update tutorialnm.h

--- tutorialnm.h
+++ tutorialnm.h
@@ -14,6 +14,7 @@ Description      : myNM.h
 
 #include "myMatrix.h"
 // Modify newton 
+extern void gradient(double x); // example
 extern double newtonRaphsonfunc(double func(const double x), double dfunc(const double x), double x0, double tol); //  newtonRaphson method 함수 헤더로 불러오기
 extern double func(const double x);
 extern double dfunc(const double x);
